44c02fe7554fad50b3a060abda821aad54d21dab
Andrés Maneiro <andres.maneiro@automattic.com>
2017-10-17 19:08:10 +0200

    Fix tests for lastactivitytimestamp reducer

--- client/state/happychat/test/reducer.js
+++ client/state/happychat/test/reducer.js
@@ -9,7 +9,10 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import { lastActivityTimestamp } from '../reducer';
-import { HAPPYCHAT_IO_SEND_MESSAGE_MESSAGE, HAPPYCHAT_RECEIVE_EVENT } from 'state/action-types';
+import {
+	HAPPYCHAT_IO_RECEIVE_MESSAGE,
+	HAPPYCHAT_IO_SEND_MESSAGE_MESSAGE,
+} from 'state/action-types';
 import { useSandbox } from 'test/helpers/use-sinon';
 
 // Simulate the time Feb 27, 2017 05:25 UTC
@@ -29,7 +32,7 @@ describe( 'reducers', () => {
 		test( 'should update on certain activity-specific actions', () => {
 			let result;
 
-			result = lastActivityTimestamp( null, { type: HAPPYCHAT_RECEIVE_EVENT } );
+			result = lastActivityTimestamp( null, { type: HAPPYCHAT_IO_RECEIVE_MESSAGE } );
 			expect( result ).to.equal( NOW );
 
 			result = lastActivityTimestamp( null, { type: HAPPYCHAT_IO_SEND_MESSAGE_MESSAGE } );
