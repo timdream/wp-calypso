48249e2cf2ed008cea02f824341d506060e55a34
Jon Surrell <jon.surrell@automattic.com>
2017-10-20 14:25:29 +0200

    Add tagged-mock component

---
+++ client/components/tagged-mock/index.js
@@ -0,0 +1,14 @@
+/** @format */
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+
+export default function makeTaggedMock( tag ) {
+	return process.env.NODE_ENV === 'test' ? () => <div data-mock={ tag } /> : returnNull;
+}
+
+function returnNull() {
+	return null;
+}
